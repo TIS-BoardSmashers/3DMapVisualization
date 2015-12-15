efb1be8dd7d80a61397707a81788f2d6a3e8055b
mrnobody2 <martin.miklis@gmail.com>
2015-12-15 01:57:03 +0100

    First version of GUI and TerrainFill

---
+++ Application/Map Vizualization/Assets/Scripts/TerrainFill.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainFill : MonoBehaviour
+{
+
+    public TerrainData tData;
+    public Terrain myTerrain;
+    public int xBase = 0;
+    public int yBase = 0;
+    public float[,] heights;
+
+    // Use this for initialization
+    void Start() {
+        int res = tData.heightmapResolution;
+        heights = new float[res, res];
+
+        for (int i = 0; i < res; i++) {
+            for (int j = 0; j < res; j++) {
+                heights[i, j] = Random.Range(0f, 0.001f);
+            }
+        }
+        tData.SetHeightsDelayLOD(xBase, yBase, heights);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}

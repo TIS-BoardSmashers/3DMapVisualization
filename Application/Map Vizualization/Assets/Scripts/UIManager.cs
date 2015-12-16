b301110d189385566c13772348a750e4513a9a02
mrnobody2 <martin.miklis@gmail.com>
2015-12-16 12:23:24 +0100

    new file browser scene

---
+++ Application/Map Vizualization/Assets/Scripts/UIManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void LoadMapButton() {
+        Application.LoadLevel("fileBrowser");
+    }
+
+    public void LoadTrackButton() {
+        Application.LoadLevel("fileBrowser");
+    }
+}
